8b280ba791f2838f03d787e47b6fde085fd79664
Acribbs <adam.cribbs@imm.ox.ac.uk>
2020-12-21 23:01:35 +0000

    pep8 fixes

--- scpipelines/pipeline_illumina.py
+++ scpipelines/pipeline_illumina.py
@@ -74,7 +74,6 @@ PARAMS = P.get_parameters(
 
 
 # Determine the location of the input fastq files
-
 try:
     PARAMS['data']
 except NameError:
@@ -93,10 +92,8 @@ SEQUENCEFILES = tuple([os.path.join(DATADIR, suffix_name)
                        for suffix_name in SEQUENCESUFFIXES])
 
 
-
-
 @follows(mkdir("split_tmp.dir"))
-@split('data.dir/*.fastq.1.gz',"split_tmp.dir/read1*")
+@split('data.dir/*.fastq.1.gz', "split_tmp.dir/read1*")
 def split_fastq_perfect(infile, outfiles):
     '''
     Split the fastq file before identifying perfect barcodes
@@ -114,15 +111,15 @@ def split_fastq_perfect(infile, outfiles):
 @follows(mkdir("perfect_reads_split.dir"))
 @transform(split_fastq_perfect,
            regex("split_tmp.dir/read1.(\S+)"),
-           [r"perfect_reads_split.dir/\1_perfect.fastq.1.gz",r"perfect_reads_split.dir/\1_error.fastq.1.gz"])
+           [r"perfect_reads_split.dir/\1_perfect.fastq.1.gz", r"perfect_reads_split.dir/\1_error.fastq.1.gz"])
 def perfect_reads(infile, outfiles):
     '''calulate perfect and error reads in fastq'''
-    
+
     read1 = infile
     read2 = infile.replace("read1.", "read2.")
 
-    name = infile.replace("read1.","")
-    name = name.replace("split_tmp.dir/","")
+    name = infile.replace("read1.", "")
+    name = name.replace("split_tmp.dir/", "")
 
     PYTHON_ROOT = os.path.join(os.path.dirname(__file__), "python/")
 
@@ -139,10 +136,10 @@ def merge_fastq_perfect(infiles, outfile):
     infile2 = []
 
     for i in infiles:
-        infile2.append(i[0].replace(".fastq.1.gz",".fastq.2.gz"))
+        infile2.append(i[0].replace(".fastq.1.gz", ".fastq.2.gz"))
         infile.append(str(i[0]))
 
-    outfile2 = outfile.replace(".fastq.1.gz",".fastq.2.gz")
+    outfile2 = outfile.replace(".fastq.1.gz", ".fastq.2.gz")
 
     infiles = " ".join(infile)
     infiles2 = " ".join(infile2)
@@ -151,6 +148,7 @@ def merge_fastq_perfect(infiles, outfile):
                    cat %(infiles2)s > %(outfile2)s'''
 
     P.run(statement)
+
 
 @follows(mkdir("whitelist.dir"))
 @transform(merge_fastq_perfect,
@@ -177,11 +175,11 @@ def correct_reads(infiles, outfile):
 
     infile1, infile2 = infiles
 
-    read1 = infile1[1].replace("_perfect.fastq.1.gz","_error.fastq.1.gz")
-    read2 = infile1[0].replace("_perfect.fastq.1.gz","_error.fastq.2.gz")
+    read1 = infile1[1].replace("_perfect.fastq.1.gz", "_error.fastq.1.gz")
+    read2 = infile1[0].replace("_perfect.fastq.1.gz", "_error.fastq.2.gz")
 
-    name = infile1[0].replace("_perfect.fastq.1.gz","")
-    name = name.replace("perfect_reads_split.dir/","")
+    name = infile1[0].replace("_perfect.fastq.1.gz", "")
+    name = name.replace("perfect_reads_split.dir/", "")
 
     distance = PARAMS['distance']
 
@@ -199,13 +197,13 @@ def merge_corrected(infiles, outfile):
     infile2 = []
 
     for i in infiles:
-        infile2.append(i.replace(".fastq.1.gz",".fastq.2.gz"))
+        infile2.append(i.replace(".fastq.1.gz", ".fastq.2.gz"))
         infile.append(str(i))
- 
+
     infiles = " ".join(infile)
     infiles2 = " ".join(infile2)
 
-    outfile2 = outfile.replace(".fastq.1.gz",".fastq.2.gz")
+    outfile2 = outfile.replace(".fastq.1.gz", ".fastq.2.gz")
 
     statement = '''cat %(infiles)s > %(outfile)s &&
                    cat %(infiles2)s > %(outfile2)s'''
@@ -222,9 +220,8 @@ def reads_toillumina(infiles, outfile):
 
     infile1, infile2 = infiles
 
-    read1 = infile1.replace(".fastq.1.gz",".fastq.2.gz")
-    read2 = infile1.replace(".fastq.1.gz",".fastq.2.gz")
-
+    read1 = infile1.replace(".fastq.1.gz", ".fastq.2.gz")
+    read2 = infile1.replace(".fastq.1.gz", ".fastq.2.gz")
 
     name = outfile.replace(".fastq.1.gz", "")
 
@@ -242,10 +239,10 @@ def merge_fastq_perfect_collapsed(infiles, outfile):
     infile2 = []
 
     for i in infiles:
-        infile2.append(i.replace(".fastq.1.gz",".fastq.2.gz"))
+        infile2.append(i.replace(".fastq.1.gz", ".fastq.2.gz"))
         infile.append(str(i))
 
-    outfile2 = outfile.replace(".fastq.1.gz",".fastq.2.gz")
+    outfile2 = outfile.replace(".fastq.1.gz", ".fastq.2.gz")
 
     infiles = " ".join(infile)
     infiles2 = " ".join(infile2)
@@ -265,8 +262,8 @@ def combine_perfect_corrected(infiles, outfile):
 
     infile_1_R1, infile_2_R1 = infiles
 
-    infile_1_R2 = infile_1_R1.replace(".fastq.1.gz",".fastq.2.gz") 
-    infile_2_R2 = infile_2_R1.replace(".fastq.1.gz",".fastq.2.gz")
+    infile_1_R2 = infile_1_R1.replace(".fastq.1.gz", ".fastq.2.gz")
+    infile_2_R2 = infile_2_R1.replace(".fastq.1.gz", ".fastq.2.gz")
 
     statement = '''cat %(infile_1_R1)s %(infile_2_R1)s > final.fastq.1.gz &&
                   cat %(infile_1_R2)s %(infile_2_R2)s > final.fastq.2.gz '''
@@ -285,6 +282,7 @@ def correct_reads(infile, outfile):
 @follows(combine_perfect_corrected)
 def full():
     pass
+
 
 def main(argv=None):
     if argv is None:
